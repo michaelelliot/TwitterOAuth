f2d9a3ff325fced650084f50010caff972efa750
thermal <michael@digital.com.au>
2011-01-21 16:04:09 +0800

    Lots of changes.

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('CONSUMER_KEY', '2KMZ22GAebECbnhqQa3lA');
-define('CONSUMER_SECRET', 'HTQP349Z1HfZdPfxt4a4NKWSj1tJhAQQSOs1PNCkqs');
-define('OAUTH_CALLBACK', 'http://localhost/twitteroauth/callback.php');
-define('TWITTER_BUTTON', '<a href="redirect.php"><img src="twitteroauthv2/twitter-light.png" alt="Sign in with Twitter"/></a>');
-?>
